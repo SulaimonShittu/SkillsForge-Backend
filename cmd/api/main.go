4013f7a82573aa658bbc16973e8e1296a19ce202
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-03-18 16:50:01 +0100

    Health checks immplemented.

---
+++ cmd/api/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", healthcheck)
+
+	err := http.ListenAndServe(":8000", mux)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func healthcheck(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		return
+	}
+	fmt.Fprintln(w, "status: available")
+	fmt.Fprintf(w, "environment: %s\n", "dev")
+	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+}

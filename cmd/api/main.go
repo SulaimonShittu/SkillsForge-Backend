eb6cd033b69875984aad5b4c1dd6f4fdf197e25c
SulaimonShittu <71654123+SulaimonShittu@users.noreply.github.com>
2025-03-19 16:49:52 +0100

    setting up config details

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,37 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
 )
 
+const version = "1.0.0"
+
+type config struct {
+	port int
+	env  string
+}
+
 func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 8080, "API server port")
+	flag.StringVar(&cfg.env, "environment", "dev", "API environment")
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/v1/healthcheck", healthcheck)
 
-	err := http.ListenAndServe(":8000", mux)
+	err := http.ListenAndServe(":8080", mux)
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+type application struct {
+	config config
+	logger *log.Logger
 }
 
 func healthcheck(w http.ResponseWriter, r *http.Request) {
@@ -23,5 +41,5 @@ func healthcheck(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintln(w, "status: available")
 	fmt.Fprintf(w, "environment: %s\n", "dev")
-	fmt.Fprintf(w, "version: %s\n", "1.0.0")
+	fmt.Fprintf(w, "version: %s\n", version)
 }
